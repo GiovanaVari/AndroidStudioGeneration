f83de5cf669487635978aa503840c3b591bf6bdb
GiovanaVari <giovana.vari.de.assis@gmail.com>
2022-09-22 22:58:45 -0300

    App dice mais bonitinho, mas da pra melhorar (22-09)

--- DiceApp/app/src/main/java/com/giovana/dice_app/MainActivity.kt
+++ DiceApp/app/src/main/java/com/giovana/dice_app/MainActivity.kt
@@ -3,8 +3,8 @@ package com.giovana.dice_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,32 +17,102 @@ class MainActivity : AppCompatActivity() {
            // Toast.makeText(this, "Botão clicado", Toast.LENGTH_LONG).show()
             rolarDados(6)
         }
-        val buttonD12 = findViewById<Button>(R.id.buttonD12)
+
+        val buttonD8 = findViewById<Button>(R.id.buttonD20)
+
+        buttonD8.setOnClickListener {
+            // Toast.makeText(this, "Botão clicado", Toast.LENGTH_LONG).show()
+            rolarDados(8)
+        }
+
+        val buttonD12 = findViewById<Button>(R.id.buttonD8)
 
         buttonD12.setOnClickListener {
             // Toast.makeText(this, "Botão clicado", Toast.LENGTH_LONG).show()
             rolarDados(12)
         }
-        val buttonD20 = findViewById<Button>(R.id.buttonD20)
+        val buttonD20 = findViewById<Button>(R.id.buttonD12)
 
         buttonD20.setOnClickListener {
             // Toast.makeText(this, "Botão clicado", Toast.LENGTH_LONG).show()
             rolarDados(20)
-        }
-        val buttonD100 = findViewById<Button>(R.id.buttonD100)
-
-        buttonD100.setOnClickListener {
-            // Toast.makeText(this, "Botão clicado", Toast.LENGTH_LONG).show()
-            rolarDados(100)
         }
     }
 
     private fun rolarDados(lados: Int){
         val rolagem = (1..lados).random()
 
-        val textDado = findViewById<TextView>(R.id.textDado)
+        //val textDado = findViewById<TextView>(R.id.textDado)
+        val imageDado = findViewById<ImageView>(R.id.imageDado)
+        //textDado.text = rolagem.toString()
 
-        textDado.text = rolagem.toString()
+        when(lados){
+
+            6 ->{
+
+                when(rolagem){
+                    1 -> imageDado.setImageResource(R.drawable.dice6_1)
+                    2 -> imageDado.setImageResource(R.drawable.dice6_2)
+                    3 -> imageDado.setImageResource(R.drawable.dice6_3)
+                    4 -> imageDado.setImageResource(R.drawable.dice6_4)
+                    5 -> imageDado.setImageResource(R.drawable.dice6_5)
+                    6 -> imageDado.setImageResource(R.drawable.dice6_6)
+                }
+            }
+            8->{
+                when(rolagem){
+                    1 -> imageDado.setImageResource(R.drawable.dice8_1)
+                    2 -> imageDado.setImageResource(R.drawable.dice8_2_png)
+                    3 -> imageDado.setImageResource(R.drawable.dice8_3_png)
+                    4 -> imageDado.setImageResource(R.drawable.dice8_4_png)
+                    5 -> imageDado.setImageResource(R.drawable.dice8_5_png)
+                    6 -> imageDado.setImageResource(R.drawable.dice8_6_png)
+                    7 -> imageDado.setImageResource(R.drawable.dice8_7_png)
+                    8 -> imageDado.setImageResource(R.drawable.dice8_8_png)
+                }
+            }
+            12 ->{
+                when(rolagem){
+                    1 -> imageDado.setImageResource(R.drawable.dice12_1)
+                    2 -> imageDado.setImageResource(R.drawable.dice12_2)
+                    3 -> imageDado.setImageResource(R.drawable.dice12_3)
+                    4 -> imageDado.setImageResource(R.drawable.dice12_4)
+                    5 -> imageDado.setImageResource(R.drawable.dice12_5)
+                    6 -> imageDado.setImageResource(R.drawable.dice12_6)
+                    7 -> imageDado.setImageResource(R.drawable.dice12_7)
+                    8 -> imageDado.setImageResource(R.drawable.dice12_8)
+                    9 -> imageDado.setImageResource(R.drawable.dice12_9)
+                    10 -> imageDado.setImageResource(R.drawable.dice12_10)
+                    11 -> imageDado.setImageResource(R.drawable.dice12_11)
+                    12 -> imageDado.setImageResource(R.drawable.dice12_12)
+                }
+                    }
+            20 ->{
+                when(rolagem){
+                    1 -> imageDado.setImageResource(R.drawable.dice20_1)
+                    2 -> imageDado.setImageResource(R.drawable.dice20_2)
+                    3 -> imageDado.setImageResource(R.drawable.dice20_3)
+                    4 -> imageDado.setImageResource(R.drawable.dice20_4)
+                    5 -> imageDado.setImageResource(R.drawable.dice20_5)
+                    6 -> imageDado.setImageResource(R.drawable.dice20_6)
+                    7 -> imageDado.setImageResource(R.drawable.dice20_7)
+                    8 -> imageDado.setImageResource(R.drawable.dice20_8)
+                    9 -> imageDado.setImageResource(R.drawable.dice20_9)
+                    10 -> imageDado.setImageResource(R.drawable.dice20_10)
+                    11 -> imageDado.setImageResource(R.drawable.dice20_11)
+                    12 -> imageDado.setImageResource(R.drawable.dice20_12)
+                    13 -> imageDado.setImageResource(R.drawable.dice20_13)
+                    14 -> imageDado.setImageResource(R.drawable.dice20_14)
+                    15 -> imageDado.setImageResource(R.drawable.dice20_15)
+                    16 -> imageDado.setImageResource(R.drawable.dice20_16)
+                    17 -> imageDado.setImageResource(R.drawable.dice20_17)
+                    18 -> imageDado.setImageResource(R.drawable.dice20_18)
+                    19 -> imageDado.setImageResource(R.drawable.dice20_19)
+                    20 -> imageDado.setImageResource(R.drawable.dice20_20)
+                }
+            }
+        }
+
 
     }
 }
