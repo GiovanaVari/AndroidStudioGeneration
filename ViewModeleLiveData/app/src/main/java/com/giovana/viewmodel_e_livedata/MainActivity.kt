49e40899967c3d0786f97bb6340413101e8d4749
GiovanaVari <giovana.vari.de.assis@gmail.com>
2022-09-28 11:24:46 -0300

    ViewModel e LiveData (28/09)

---
+++ ViewModeleLiveData/app/src/main/java/com/giovana/viewmodel_e_livedata/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.giovana.viewmodel_e_livedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.ViewModelInitializer
+import com.giovana.viewmodel_e_livedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mainViewModel = ViewModelProvider(this)
+            .get(MainViewModel::class.java)
+
+        mainViewModel.cont.observe(this) {
+            binding.textResultado.text = it.toString()
+        }
+        binding.buttonClique.setOnClickListener {
+            mainViewModel.addNum()
+        }
+
+    }
+}
